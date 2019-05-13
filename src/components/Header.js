6144953e50d9ca54a152b9aba1a0d9ae29301121
emily baier <emilyrbaier@gmail.com>
2019-05-13 16:31:20 -0700

    we have a pup

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <>
+    <img src="../../assets/saturday.jpg"></img>
+    <h1>THE COOLEST DOG</h1>
+    </>
+  );
+}
